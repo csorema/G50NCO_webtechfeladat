061cf8c92ff6f673b9008c5f7f57dd1fbb4b0de8
CsMargareta <72508599+csorema@users.noreply.github.com>
2023-12-16 19:31:30 +0100

    Add files via upload

---
+++ Webtechfeladat/fadeInAnimation.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Az oldal tartalmának kiválasztása
+    var mainContent = document.getElementById('orokbefogadas');
+  
+    // Hozzáadja az animációs osztályt az oldal tartalmához
+    mainContent.classList.add('fade-in');
+  });
---
+++ Webtechfeladat/popup.js
@@ -0,0 +1,13 @@
+function showPopup() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('popup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('popup').style.display = 'none';
+}
+
+function submitForm() {
+    showPopup();
+}
