c57467753650d4cf253881f4f4db95f488786316
CsMargareta <72508599+csorema@users.noreply.github.com>
2023-12-18 12:34:33 +0100

    Add files via upload

--- Webtechfeladat/fadeInAnimation.js
+++ Webtechfeladat/fadeInAnimation.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Az oldal tartalmának kiválasztása
     var mainContent = document.getElementById('orokbefogadas');
   
-    // Hozzáadja az animációs osztályt az oldal tartalmához
     mainContent.classList.add('fade-in');
   });
